4a28628216c988d7889d9839cc18287ae062cb81
ALOHADAWN <llezezel@gmail.com>
2026-06-22 18:54:53 +0500

    Попытка пофиксить баг с character preview
    Попытка пофиксить баг с character preview

--- tgui/packages/tgui/interfaces/common/CharacterPreview.tsx
+++ tgui/packages/tgui/interfaces/common/CharacterPreview.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { ByondUi } from 'tgui-core/components';
 
 export const CharacterPreview = (props: {
@@ -6,14 +7,68 @@ export const CharacterPreview = (props: {
   id: string | null;
 }) => {
   const { width = '272px' } = props;
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!props.id) {
+      return;
+    }
+
+    let timeoutId: ReturnType<typeof setTimeout> | undefined;
+    let rafId: number | undefined;
+    let resizeObserver: ResizeObserver | undefined;
+
+    const notifyResize = () => {
+      window.dispatchEvent(new Event('resize'));
+    };
+
+    rafId = requestAnimationFrame(() => {
+      requestAnimationFrame(notifyResize);
+    });
+    timeoutId = setTimeout(notifyResize, 150);
+
+    if (wrapperRef.current && 'ResizeObserver' in window) {
+      resizeObserver = new ResizeObserver(notifyResize);
+      resizeObserver.observe(wrapperRef.current);
+    }
+
+    return () => {
+      if (rafId !== undefined) {
+        cancelAnimationFrame(rafId);
+      }
+      if (timeoutId !== undefined) {
+        clearTimeout(timeoutId);
+      }
+      resizeObserver?.disconnect();
+    };
+  }, [props.id]);
+
   return (
-    <ByondUi
-      width={width}
-      height={props.height}
-      params={{
-        id: props.id ?? undefined,
-        type: 'map',
+    <div
+      ref={wrapperRef}
+      style={{
+        width,
+        height: props.height,
+        maxWidth: '100%',
+        minWidth: 0,
+        minHeight: 0,
+        boxSizing: 'border-box',
+        overflow: 'hidden',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
-    />
+    >
+      {props.id ? (
+        <ByondUi
+          width="100%"
+          height="100%"
+          params={{
+            id: props.id,
+            type: 'map',
+          }}
+        />
+      ) : null}
+    </div>
   );
 };
